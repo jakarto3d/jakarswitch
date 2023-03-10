c19ea6354accccfbbe61491cc3f0159810b983f3
Loic Messal <15694700+Tofull@users.noreply.github.com>
2023-03-09 23:08:10 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# JakarSwitch
+
+JakarSwitch is an extension for Google Chrome that makes it easy to teleport between Jakartowns and Google Maps. Using this extension, you can quickly navigate between the two maps to discover places and points of interest.
+
+## Demo
+![demo](doc/demo_jakarswitch.gif)
+
+## Authors
+- ideas came from Loïc Messal
+- draft implementation directly from ChatGPT (really awesome!)
+- some enhancements by Loïc Messal to make it work bidirectionnal
+
+## Etymology
+
+🤣
+
+![chatgpt](doc/etymology.png)
---
+++ chrome_extension/popup.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+   // Code qui sera exécuté lorsque le DOM sera chargé
+
+   // Récupération de l'URL de la page actuelle
+   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      var url = tabs[0].url;
+
+      // Extraction de la latitude et de la longitude de l'URL
+      var google_localization_encoded_match = url.match(/@([-0-9.]+),([-0-9.]+)/);
+      if (google_localization_encoded_match) {
+         var latitude = google_localization_encoded_match[1];
+         var longitude = google_localization_encoded_match[2];
+
+         // Aller sur Jakartowns avec les coordonnées récupérées
+         var newUrl = `https://maps.jakarto.com?lat=${latitude}&lng=${longitude}`;
+
+         // Ouverture de la nouvelle URL dans un nouvel onglet
+         chrome.tabs.create({ url: newUrl });
+         return
+      }
+
+      var jktowns_localization_encoded_match = url.match(/^https:\/\/maps\.jakarto\.com\/\?.*?(?:lat|latitude)=([-0-9.]+).*?(?:lng|long|longitude)=([-0-9.]+)/i);
+      if (jktowns_localization_encoded_match) {
+         var latitude = jktowns_localization_encoded_match[1];
+         var longitude = jktowns_localization_encoded_match[2];
+
+         // Aller sur Jakartowns avec les coordonnées récupérées
+         var newUrl = `https://www.google.com/maps/@${latitude},${longitude},18z`;
+
+         // Ouverture de la nouvelle URL dans un nouvel onglet
+         chrome.tabs.create({ url: newUrl });
+         return
+      }
+
+   });
+});
